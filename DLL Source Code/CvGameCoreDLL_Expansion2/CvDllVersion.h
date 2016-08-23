5fb44c524a085fb12601f1d998f1b0f6252479c4
DelnarErsike <delnar.ersike@gmail.com>
2016-08-23 05:01:09 +0100

    Initial Commit

---
+++ DLL Source Code/CvGameCoreDLL_Expansion2/CvDllVersion.h
@@ -0,0 +1,26 @@
+/*	-------------------------------------------------------------------------------------------------------
+	© 1991-2012 Take-Two Interactive Software and its subsidiaries.  Developed by Firaxis Games.  
+	Sid Meier's Civilization V, Civ, Civilization, 2K Games, Firaxis Games, Take-Two Interactive Software 
+	and their respective logos are all trademarks of Take-Two interactive Software, Inc.  
+	All other marks and trademarks are the property of their respective owners.  
+	All rights reserved. 
+	------------------------------------------------------------------------------------------------------- */
+#pragma once
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// WARNING! This must be unique for each DLL!
+// If you are a modder and you are creating your own DLL from the original source, this must be changed to a unique GUID.
+// Use the Visual Studio Create GUID option in the Tools menu to create a new GUID.
+// {C3B2B6B3-439C-480b-8536-1CF39FCFC682}
+// {83F34FF0-3D60-43e7-AEDB-82C79867EE3F}
+static const GUID CIV5_XP2_DLL_GUID = 
+//{ 0x83f34ff0, 0x3d60, 0x43e7, { 0xae, 0xdb, 0x82, 0xc7, 0x98, 0x67, 0xee, 0x3f } };
+
+// {68BD15DB-F6BA-4AF8-825D-67107F55C412}
+{ 0x68bd15db, 0xf6ba, 0x4af8, { 0x82, 0x5d, 0x67, 0x10, 0x7f, 0x55, 0xc4, 0x12 } };
+
+
+static const char* CIV5_XP2_DLL_VERSION = "1.0.0";
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
---
+++ DLL Source Code/CvGameCoreDLL_Expansion2/_Defines.h
@@ -0,0 +1,11 @@
+// Defines.h
+#pragma once
+
+#ifndef DEFINES_LIST
+// List of defines that are supposed to be for new additions and can be easily toggled on and off (plus they help with merging)
+#define DEFINES_LIST
+
+// Adds ranged counterattacks to the game
+#define DEL_RANGED_COUNTERATTACKS
+
+#endif
